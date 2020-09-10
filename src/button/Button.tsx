d4f30d844cc2941df14faace82ea21f4818a4ddb
Amir Kiumarsi <codacy20@users.noreply.github.com>
2020-09-10 23:45:50 +0200

    btn styling based on state

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
+import "./Button.scss";
 
 export default function Button(props: any) {
   if (props.requested === 1)
     return <button onClick={props.submit}>Sending...</button>;
   else if (props.requested === 2)
-    return <button onClick={props.submit}>Sent</button>;
+    return (
+      <button className="sent" onClick={props.submit}>
+        Sent
+      </button>
+    );
   else return <button onClick={props.submit}>Submit</button>;
 }
