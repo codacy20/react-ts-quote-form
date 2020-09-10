463a3b6f3ecd66d9b9fc5651bb172f6170e15aa9
Amir Kiumarsi <codacy20@users.noreply.github.com>
2020-09-10 22:20:44 +0200

    styling of input fields

--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -44,41 +44,56 @@ export default function Form() {
   }
 
   return (
-    <div>
-      <span>How many people registered? {data.length} so far!</span>
-      <div>
-        <label htmlFor="one">name</label>
-        <input type="text" id="one" onChange={(e) => setName(e.target.value)} />
+    <div className="form-container">
+      <span>How many people registered? {data?.length} so far!</span>
+      <div className="field">
+        <label htmlFor="one">Hey, my name is </label>
+        <input
+          type="text"
+          id="one"
+          autoComplete="off"
+          onFocus={(placeholder = "")}
+          placeholder={data[0]?.name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
-      <div>
-        <label htmlFor="two">email</label>
+      <div className="field">
+        <label htmlFor="two">My email address is</label>
         <input
           type="text"
           id="two"
+          autoComplete="off"
+          placeholder={data[0]?.email}
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
-      <div>
-        <label htmlFor="three">ask</label>
+      <div className="field">
+        <label htmlFor="three">I am looking for help with a</label>
         <input
           type="text"
           id="three"
+          autoComplete="off"
+          placeholder={data[0]?.ask}
           onChange={(e) => setAsk(e.target.value)}
         />
       </div>
-      <div>
-        <label htmlFor="four">budget</label>
+      <div className="field">
+        <label htmlFor="four">My maximum budget is $</label>
         <input
           type="text"
           id="four"
+          autoComplete="off"
+          placeholder={data[0]?.budget}
           onChange={(e) => setBudget(Number(e.target.value))}
         />
       </div>
-      <div>
-        <label htmlFor="five">time</label>
+      <div className="field">
+        <label htmlFor="five">I need my project within</label>
         <input
           type="text"
           id="five"
+          autoComplete="off"
+          placeholder={data[0]?.time}
           onChange={(e) => setTime(Number(e.target.value))}
         />
       </div>
